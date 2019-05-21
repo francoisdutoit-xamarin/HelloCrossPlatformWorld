e542803dbeca1ef05daa495200c8561590329761
Zonicblue <Francois.duToit@yahoo.com>
2019-05-21 19:36:58 +0200

    Add project files.

---
+++ HelloCrossPlatformWorld/HelloCrossPlatformWorld.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace HelloCrossPlatformWorld.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
